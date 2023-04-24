b61938294b815c384cca5935924f454be0f64bae
LLltirlec <fhntvrf1998@gmail.com>
2023-04-24 11:15:03 +0300

    code for traffic light

--- trafficLight/ViewController.swift
+++ trafficLight/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    @IBOutlet weak var stepBtn: UIButton!
+    
+    private var radius: CGFloat = 90
+    private var state = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLight.layer.cornerRadius = radius
+        yellowLight.layer.cornerRadius = radius
+        greenLight.layer.cornerRadius = radius
     }
 
-
+    @IBAction func stepBtnTapped() {
+        stepBtn.setTitle("Next", for: .normal)
+        switch state{
+        case 0:
+            state += 1
+            greenLight.alpha = 0.65
+            redLight.alpha = 1
+            break
+        case 1:
+            state += 1
+            redLight.alpha = 0.65
+            yellowLight.alpha = 1
+            break
+        case 2:
+            state = 0
+            yellowLight.alpha = 0.65
+            greenLight.alpha = 1
+            break
+        default: print("error, incorrect state")
+        }
+    }
+    
 }
 
